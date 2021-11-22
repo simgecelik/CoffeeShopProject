f002da0406203d9d9caef65f0746791a5c0a67f2
simgecelik <73402139+simgecelik@users.noreply.github.com>
2021-11-22 18:30:07 +0300

    Update MernisServiceAdapters.java

--- src/Adapters/MernisServiceAdapters.java
+++ src/Adapters/MernisServiceAdapters.java
@@ -9,7 +9,7 @@ import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 public class MernisServiceAdapters implements ICustomerCheckService {
 	public boolean result;
-	public boolean CheckIfRealPerson(Customer customer) {
+	public boolean checkIfRealPerson(Customer customer) {
 		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
 		try {
 			return result = kpsPublicSoapProxy.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()), customer.getFirstName(), customer.getLastName(), customer.getDateOfBirth());
